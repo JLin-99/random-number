73fe2309a91855e42c953bc36bbc64b23ffaba76
José Lin (Light-it) <jose.lin@lightit.io>
2023-05-12 18:49:45 -0300

    feature: add TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useReducer, useState } from "react";
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
 const getRandomNumberFromAPI = async (): Promise<number> => {
   const res = await fetch(
@@ -11,34 +12,19 @@ const getRandomNumberFromAPI = async (): Promise<number> => {
 };
 
 export const App = () => {
-  const [number, setNumber] = useState<number>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>();
-
-  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
-
-  useEffect(() => {
-    setIsLoading(true);
-    getRandomNumberFromAPI()
-      .then(setNumber)
-      .catch((error) => setError(error.message));
-  }, [key]);
-
-  useEffect(() => {
-    if (number) setIsLoading(false);
-  }, [number]);
-
-  useEffect(() => {
-    if (error) setIsLoading(false);
-  }, [error]);
+  const query = useQuery(["randomNumber"], getRandomNumberFromAPI);
 
   return (
     <div>
-      {isLoading ? <h2>Loading...</h2> : <h1>Random number: {number}</h1>}
-      {!isLoading && error && <h3>{error}</h3>}
+      {query.isFetching ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>Random number: {query.data}</h1>
+      )}
+      {!query.isLoading && query.isError && <h3>{`${query.error}`}</h3>}
 
-      <button onClick={forceRefetch} disabled={isLoading}>
-        {isLoading ? "..." : "New number"}
+      <button onClick={() => query.refetch()} disabled={query.isFetching}>
+        {query.isFetching ? "..." : "New number"}
       </button>
     </div>
   );
